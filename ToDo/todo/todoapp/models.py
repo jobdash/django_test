07f7ec531d7f109f5bcd637102253266442afdb4
kelseyejacobsen@gmail.com <kelseyejacobsen@gmail.com>
2014-11-02 16:00:41 -0700

    Updated Task Model

--- ToDo/todo/todoapp/models.py
+++ ToDo/todo/todoapp/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# Task model - text input to a todo item
+# Task model - text input for a ToDo item
+class Task(models.Model):
+    task_text = models.CharField(max_length=75)
+    task_status = models.BooleanField(default=False) #mark as done / default incomplete
+    def __str__(self):
+        return self.task_text
+
 # list of unfinished todos
-# mark a task as done
