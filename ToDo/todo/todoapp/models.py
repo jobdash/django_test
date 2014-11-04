c944c7aa49406dbede717b6080b09d787c7a6e39
kelseyejacobsen@gmail.com <kelseyejacobsen@gmail.com>
2014-11-03 22:00:33 -0700

    created admin

--- ToDo/todo/todoapp/models.py
+++ ToDo/todo/todoapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 # Task model - text input for a ToDo item
 class Task(models.Model):
-    task_text = models.CharField(max_length=75)
-    task_status = models.BooleanField(default=False) #mark as done / default incomplete
+    task_text = models.CharField(max_length=75)         #input for the task
+    task_status = models.BooleanField(default=False)    #mark as done / default incomplete
     def __str__(self):
         return self.task_text
 
