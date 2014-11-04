cf8f0b4b62ad5d7ed997a8837e2c67c4445ebad4
Michael Anderton <michael.anderton15@gmail.com>
2014-11-04 15:13:23 -0700

    added the ToDoList app to ToDo

---
+++ ToDo/ToDoList/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+	task_complete = models.BooleanField(default=False)
