98fa0fed7426a8208a734d2faa055427cdc3dcb2
kelseyejacobsen@gmail.com <kelseyejacobsen@gmail.com>
2014-11-04 11:13:29 -0700

    Html and CSS added

--- ToDo/todo/todoapp/views.py
+++ ToDo/todo/todoapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Task
 
 # creating the view
 
 def index(request):
     return HttpResponse('Hello JobDash, you are at Kelsey Jacobsen\'s ToDo app')
+
+def index(request):
+    # lists of tasks
+    tasks = Task.objects.all()
+    # loads the index.html template
+    context = {'tasks': tasks}
+    return render(request, 'todoapp/index.html', context)
